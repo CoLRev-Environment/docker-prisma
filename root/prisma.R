d355ea2c34f579c1697d2c52b97f61a37014273f
Gerit Wagner <geritwagner@users.noreply.github.com>
2022-10-29 11:13:19 +0200

    Don't show other methods by default

--- root/prisma.R
+++ root/prisma.R
@@ -7,5 +7,5 @@ output_file = args[2]
 
 flow_data <- read.csv(input_file)
 flow_data <- PRISMA_data(flow_data)
-flow <- PRISMA_flowdiagram(flow_data, fontsize = 12, interactive = TRUE, previous = FALSE, other = TRUE)
+flow <- PRISMA_flowdiagram(flow_data, fontsize = 12, interactive = TRUE, previous = FALSE, other = FALSE)
 PRISMA_save(flow, filename = output_file, filetype = NA, overwrite = TRUE)
